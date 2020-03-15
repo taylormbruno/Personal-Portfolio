4c84621973184afc6e1b7e27a0153007e898ab46
Taylor Bruno <taylorbruno895@gmail.com>
2020-03-14 23:01:32 -0400

    updated styling for contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,8 +4,9 @@ import Container from "../components/Container";
 function Contact() {
     return(
         <Container>
-            <div className="row">
-                <div className="col-md">
+            <div className="row mt-3">
+
+                <div className="col-md mr-3">
                     <h2 id="contactHead">Get in touch</h2>
                     <form action="https://formspree.io/mbjlqyrw" method="POST">
                         <label>Name: <input type="text" name="name" /></label><br />
@@ -14,22 +15,45 @@ function Contact() {
                         <button type="submit" className="btn btn-light">Send</button>
                     </form>
                 </div>
-                <div className="col-md contactLinks">
-                    <a href="mailto:taylormareebruno@gmail.com">
-                        <i class="fas fa-envelope">Email</i>
-                    </a> 
-                    <a href="tel:828-449-9781" >
-                        <i class="fas fa-phone-square">Phone</i>
-                    </a>
-                    <a href="/assets/images/resumePDF.pdf"  target="_blank" rel="noopener noreferrer">
-                        <i class="fas fa-file">Resume</i>
-                    </a>
-                    <a href="https://github.com/taylormbruno"  target="_blank" rel="noopener noreferrer">
-                        <i class="fab fa-github">Github</i>
-                    </a>
-                    <a href="https://www.linkedin.com/in/taylor-bruno-5b5a49189/"  target="_blank" rel="noopener noreferrer">
-                        <i class="fab fa-linkedin">LinkedIn</i>
-                    </a>
+
+                <div className="col-md contactLinks ml-3">
+                    <div className="contactBtn">
+                        <a href="mailto:taylormareebruno@gmail.com">
+                            <i className="fas fa-envelope fa-2x contactIcon"></i><br />
+                            <span className="cLink">Email</span>
+                        </a>
+                    </div>
+                    
+                    <div className="contactBtn">
+                        <a href="tel:828-449-9781">
+                            <i className="fas fa-phone-square fa-2x contactIcon"></i><br />
+                            <span className="cLink">Phone</span>
+                        </a>
+                    </div>
+
+                    
+                    <div className="contactBtn">
+                        <a href="/assets/images/resumePDF.pdf"  target="_blank" rel="noopener noreferrer">
+                            <i className="fas fa-file fa-2x contactIcon"></i><br />
+                            <span className="cLink">Resume</span>
+                        </a>
+                    </div>
+
+                    
+                    <div className="contactBtn">
+                        <a href="https://github.com/taylormbruno"  target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-github fa-2x contactIcon"></i><br />
+                            <span className="cLink">Github</span>
+                        </a>
+                    </div>
+
+                    
+                    <div className="contactBtn">
+                        <a href="https://www.linkedin.com/in/taylor-bruno-5b5a49189/"  target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-linkedin fa-2x contactIcon"></i><br />
+                            <span className="cLink">LinkedIn</span>
+                        </a>
+                    </div>  
                 </div>
             </div>
         </Container>
