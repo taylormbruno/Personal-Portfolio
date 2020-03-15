de97b2982914a0311736bfc5c69f57ee3dee8915
Taylor Bruno <taylorbruno895@gmail.com>
2020-03-14 23:09:26 -0400

    removed unnecessary styling and ids

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,17 +4,16 @@ import Container from "../components/Container";
 function About() {
     return (
         <Container>
-            <div className="row m-2 p-4" id="headerEl">
+            <div className="row m-2 p-4">
                 <div className="col-md">
                     <img src="/assets/images/hw-photo.jpg" id="aboutImg" alt="Taylor Bruno" />    
                 </div>
                 <div className="col-md">  
-                    
-                    <h3 id="bio">Full-Stack Web Developer passionate about approaching challenges from all angles and excited to collaborate with others to create meaningful web applications. Recently earned a certificate in Full Stack Development from the University of North Carolina - Charlotte.</h3>
+                    <h3>Full-Stack Web Developer passionate about approaching challenges from all angles and excited to collaborate with others to create meaningful web applications. Recently earned a certificate in Full Stack Development from the University of North Carolina - Charlotte.</h3>
                 </div>
             </div>
-            <div className="skillList text-left" id="skillSet">
-                <h4 className="skillHead p-2">Technical Skills</h4> 
+            <div className="skillList text-left">
+                <h4 className="p-2">Technical Skills</h4> 
                 <div className="row"> 
                     <div className="col-md">
                         <ul>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,6 @@ function Contact() {
     return(
         <Container>
             <div className="row mt-3">
-
                 <div className="col-md mr-3">
                     <h2 id="contactHead">Get in touch</h2>
                     <form action="https://formspree.io/mbjlqyrw" method="POST">
@@ -15,7 +14,6 @@ function Contact() {
                         <button type="submit" className="btn btn-light">Send</button>
                     </form>
                 </div>
-
                 <div className="col-md contactLinks ml-3">
                     <div className="contactBtn">
                         <a href="mailto:taylormareebruno@gmail.com">
@@ -23,31 +21,24 @@ function Contact() {
                             <span className="cLink">Email</span>
                         </a>
                     </div>
-                    
                     <div className="contactBtn">
                         <a href="tel:828-449-9781">
                             <i className="fas fa-phone-square fa-2x contactIcon"></i><br />
                             <span className="cLink">Phone</span>
                         </a>
                     </div>
-
-                    
                     <div className="contactBtn">
                         <a href="/assets/images/resumePDF.pdf"  target="_blank" rel="noopener noreferrer">
                             <i className="fas fa-file fa-2x contactIcon"></i><br />
                             <span className="cLink">Resume</span>
                         </a>
-                    </div>
-
-                    
+                    </div>  
                     <div className="contactBtn">
                         <a href="https://github.com/taylormbruno"  target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-github fa-2x contactIcon"></i><br />
                             <span className="cLink">Github</span>
                         </a>
                     </div>
-
-                    
                     <div className="contactBtn">
                         <a href="https://www.linkedin.com/in/taylor-bruno-5b5a49189/"  target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-linkedin fa-2x contactIcon"></i><br />
